5308a5eed1b21ac514f4bef4cfe2787b74e1a51d
AntoCic <anto.cic.127@gmail.com>
2024-05-22 15:54:33 +0200

    added base assets

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,7 +4,7 @@
 
 @section('content')
 <h1>Home</h1>
-<p>
+<p class="text-white m-5">
     Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit provident fugit aliquam incidunt consectetur enim voluptatibus! Recusandae debitis iusto corporis eligendi, officia dolore aperiam voluptatem excepturi cumque. Impedit, perferendis fuga?
 </p>
 @endsection
